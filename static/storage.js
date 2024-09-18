66b81f8ed9d24a8a125a725dfa5b7d8d56e8da2d
harryoltmans <harry@adeninelabs.com>
2024-09-18 12:53:30 -0700

    added some stuff

--- static/storage.js
+++ static/storage.js
@@ -1,6 +1,8 @@
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
-    localStorage.setItem('darkMode', document.body.classList.contains('dark-mode'));
+    const darkModeEnabled = document.body.classList.contains('dark-mode');
+    localStorage.setItem('darkMode', darkModeEnabled);
+    updateSliderIcon(darkModeEnabled);
 }
 
 function applyDarkModePreference() {
@@ -8,6 +10,18 @@ function applyDarkModePreference() {
     if (darkModeEnabled) {
         document.body.classList.add('dark-mode');
         document.getElementById('dark-mode-switch').checked = true;
+    }
+    updateSliderIcon(darkModeEnabled);
+}
+
+function updateSliderIcon(darkModeEnabled) {
+    const sliderIcon = document.getElementById('slider-icon');
+    if (darkModeEnabled) {
+        sliderIcon.src = "{{ url_for('static', filename='assets/moon.svg') }}";
+        sliderIcon.alt = "Moon Icon";
+    } else {
+        sliderIcon.src = "{{ url_for('static', filename='assets/sun.svg') }}";
+        sliderIcon.alt = "Sun Icon";
     }
 }
 

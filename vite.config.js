b36eefe277f235d17063fe0d0784c9c59d20c9b7
harryoltmans <harry@adeninelabs.com>
2024-10-02 08:47:54 -0700

    minor testing and small update to app.py to fix error

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: '/dist/**',
-  },
-  root: 'frontend',
-  publicDir: 'public', // Static files served from `frontend/public`
-});
+export default {
+  server: {
+    proxy: {
+      '/calculate': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+      '/history': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  }
+};
